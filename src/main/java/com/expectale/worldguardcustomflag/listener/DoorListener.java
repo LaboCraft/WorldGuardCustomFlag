8ae0e459ca4d3f037bfaf4ba7e096eaeabfda873
Nathan <nathan.fain@epitech.eu>
2023-03-29 19:37:50 +0200

    update doors list

--- src/main/java/com/expectale/worldguardcustomflag/listener/DoorListener.java
+++ src/main/java/com/expectale/worldguardcustomflag/listener/DoorListener.java
@@ -8,15 +8,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 
 public class DoorListener implements Listener {
+    private final HashSet<Material> doors = new HashSet<>();
 
-    private final Set<Material> doors = new HashSet<>(Arrays.asList(    Material.ACACIA_DOOR, Material.BIRCH_DOOR, Material.CRIMSON_DOOR, Material.JUNGLE_DOOR, Material.DARK_OAK_DOOR, Material.OAK_DOOR, Material.SPRUCE_DOOR, Material.WARPED_DOOR,
-                                                                        Material.ACACIA_TRAPDOOR, Material.BIRCH_TRAPDOOR, Material.CRIMSON_TRAPDOOR, Material.JUNGLE_TRAPDOOR, Material.DARK_OAK_TRAPDOOR, Material.OAK_TRAPDOOR, Material.SPRUCE_TRAPDOOR, Material.WARPED_TRAPDOOR,
-                                                                        Material.ACACIA_FENCE_GATE, Material.BIRCH_FENCE_GATE, Material.CRIMSON_FENCE_GATE, Material.JUNGLE_FENCE_GATE, Material.DARK_OAK_FENCE_GATE, Material.OAK_FENCE_GATE, Material.SPRUCE_FENCE_GATE, Material.WARPED_FENCE_GATE));
+    public DoorListener() {
+        super();
+        for (Material material : Material.values()) {
+            if (material.toString().toUpperCase().contains("_DOOR") || material.toString().toUpperCase().contains("_TRAPDOOR") || material.toString().toUpperCase().contains("_FENCE_GATE")) {
+                doors.add(material);
+            }
+        }
+    }
 
     @EventHandler
     public void onPlayerInteractEvent(PlayerInteractEvent event) {
